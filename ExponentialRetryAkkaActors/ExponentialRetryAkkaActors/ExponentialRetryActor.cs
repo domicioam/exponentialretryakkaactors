adb4d848c9c655187dca8105536b0557167476e8
Domício Medeiros <domicioam@gmail.com>
2021-06-19 09:24:37 +0100

    Initial version of retry logic

--- ExponentialRetryAkkaActors/ExponentialRetryAkkaActors/ExponentialRetryActor.cs
+++ ExponentialRetryAkkaActors/ExponentialRetryAkkaActors/ExponentialRetryActor.cs
@@ -1,13 +1,80 @@
 ﻿using Akka.Actor;
+using Akka.Pattern;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Akka.Actor.Status;
 
 namespace ExponentialRetryAkkaActors
 {
+    public delegate void Work();
+
     public class ExponentialRetryActor : ReceiveActor
     {
+        #region Messages
+        public class DoWork
+        {
+            public Work Work { get; }
+
+            public DoWork(Work work)
+            {
+                Work = work;
+            }
+        }
+        #endregion
+
+        public ExponentialRetryActor()
+        {
+            Receive<DoWork>(msg =>
+            {
+                requestor = Sender;
+                var workerProps = Props.Create(() => new Worker(msg.Work));
+
+                var supervisorProps = BackoffSupervisor.Props(
+                    Backoff.OnFailure(
+                        workerProps,
+                        childName: "myEcho",
+                        minBackoff: TimeSpan.FromSeconds(3),
+                        maxBackoff: TimeSpan.FromSeconds(30),
+                        randomFactor: 0.2,
+                        maxNrOfRetries: 10)
+                        .WithSupervisorStrategy(new OneForOneStrategy(exception =>
+                        {
+                            if (exception is Exception)
+                                return Directive.Restart;
+                            return Directive.Escalate;
+                        })));
+
+                supervisor = Context.ActorOf(supervisorProps, "supervisor");
+            });
+
+            Receive<Success>(msg =>
+            {
+                Context.Stop(supervisor);
+                requestor.Tell(msg);
+            });
+        }
+
+        private IActorRef requestor;
+        private IActorRef supervisor;
+
+        private class Worker : ReceiveActor
+        {
+            private readonly Work work;
+
+            public Worker(Work work)
+            {
+                this.work = work;
+            }
+
+            protected override void PreStart()
+            {
+                base.PreStart();
+                work();
+                Context.Parent.Tell(new Success(null));
+            }
+        }
     }
 }
--- ExponentialRetryAkkaActors/ExponentialRetryAkkaActorsTests/ExponentialRetryActorTests.cs
+++ ExponentialRetryAkkaActors/ExponentialRetryAkkaActorsTests/ExponentialRetryActorTests.cs
@@ -1,7 +1,10 @@
 ﻿using Akka.Actor;
 using Akka.TestKit.Xunit2;
 using ExponentialRetryAkkaActors;
+using System;
+using System.Diagnostics;
 using Xunit;
+using static Akka.Actor.Status;
 
 namespace ExponentialRetryAkkaActorsTests
 {
@@ -11,6 +14,14 @@ namespace ExponentialRetryAkkaActorsTests
         public void Should_create_actor()
         {
             var subject = Sys.ActorOf(Props.Create(() => new ExponentialRetryActor()));
+        }
+
+        [Fact]
+        public void Should_do_work()
+        {
+            var subject = Sys.ActorOf(Props.Create(() => new ExponentialRetryActor()));
+            subject.Tell(new ExponentialRetryActor.DoWork(() => Debug.WriteLine("Hello world")));
+            ExpectMsg<Success>();
         }
     }
 }
