a9b3aac5fc067ee3e446a73c35c0d35ff033e263
Domício Medeiros <domicioam@gmail.com>
2021-06-19 08:31:44 +0100

    Add first unit test

---
+++ ExponentialRetryAkkaActors/ExponentialRetryAkkaActorsTests/ExponentialRetryActorTests.cs
@@ -0,0 +1,16 @@
+﻿using Akka.Actor;
+using Akka.TestKit.Xunit2;
+using ExponentialRetryAkkaActors;
+using Xunit;
+
+namespace ExponentialRetryAkkaActorsTests
+{
+    public class ExponentialRetryActorTests : TestKit
+    {
+        [Fact]
+        public void Should_create_actor()
+        {
+            var subject = Sys.ActorOf(Props.Create(() => new ExponentialRetryActor()));
+        }
+    }
+}
